0ea1051033d2ab915307eebb1030ea49de1e2f87
Patryk Wichrowski <wichrowski.patryk92@gmail.com>
2019-02-03 19:30:52 +0100

    Add Header component

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -4,6 +4,7 @@ class Main extends Component {
     render() {
         return (
             <div className="Main">
+                <h2>Header</h2>
                 <h2>Main component</h2>
             </div>
         );
