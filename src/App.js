59029abbb8b8c04d77e70371da70f49bc7225ef6
Patryk Wichrowski <wichrowski.patryk92@gmail.com>
2019-02-03 12:26:51 +0100

    Add ant design

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>{this.props.testInfo.test                                              }</h1>
+        <h1>{this.props.testInfo.test}</h1>
       </div>
     );
   }
