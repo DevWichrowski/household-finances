e5010d2073e130965bd4369d313c9e6a9f0c884d
Patryk Wichrowski <wichrowski.patryk92@gmail.com>
2019-02-04 19:35:29 +0100

    Refactor inline styles to scss.

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,25 +1,29 @@
 import React, {Component} from 'react';
 import {Layout, Menu, Breadcrumb, Icon} from 'antd';
 import 'antd/dist/antd.css';
-import SubMenu from "antd/es/menu/SubMenu";
+import './Main.scss';
 
 
 class Main extends Component {
-    state = {
-        collapsed: false,
-    };
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            collapsed: false,
+        };
+    }
 
     onCollapse = (collapsed) => {
         console.log(collapsed);
         this.setState({collapsed});
-    }
+    };
 
     render() {
-        const {Header, Content, Footer, Sider,} = Layout;
+        const {Content, Footer, Sider,} = Layout;
 
         return (
             <div className="Main">
-                <Layout style={{minHeight: '100vh'}}>
+                <Layout className="layout">
                     <Sider
                         collapsible
                         collapsed={this.state.collapsed}
@@ -28,44 +32,22 @@ class Main extends Component {
                         <div className="logo"/>
                         <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
                             <Menu.Item key="1">
-                                <Icon type="pie-chart"/>
-                                <span>Option 1</span>
+                                <Icon type="credit-card"/>
+                                <span>Stan konta</span>
                             </Menu.Item>
                             <Menu.Item key="2">
-                                <Icon type="desktop"/>
-                                <span>Option 2</span>
+                                <Icon type="line-chart"/>
+                                <span>Historia</span>
                             </Menu.Item>
-                            <SubMenu
-                                key="sub1"
-                                title={<span><Icon type="user"/><span>User</span></span>}
-                            >
-                                <Menu.Item key="3">Tom</Menu.Item>
-                                <Menu.Item key="4">Bill</Menu.Item>
-                                <Menu.Item key="5">Alex</Menu.Item>
-                            </SubMenu>
-                            <SubMenu
-                                key="sub2"
-                                title={<span><Icon type="team"/><span>Team</span></span>}
-                            >
-                                <Menu.Item key="6">Team 1</Menu.Item>
-                                <Menu.Item key="8">Team 2</Menu.Item>
-                            </SubMenu>
-                            <Menu.Item key="9">
-                                <Icon type="file"/>
-                                <span>File</span>
+                            <Menu.Item key="3">
+                                <Icon type="star"/>
+                                <span>Cele</span>
                             </Menu.Item>
                         </Menu>
                     </Sider>
                     <Layout>
-                        <Header style={{background: '#fff', padding: 0}}/>
-                        <Content style={{margin: '0 16px'}}>
-                            <Breadcrumb style={{margin: '16px 0'}}>
-                                <Breadcrumb.Item>User</Breadcrumb.Item>
-                                <Breadcrumb.Item>Bill</Breadcrumb.Item>
-                            </Breadcrumb>
-                            <div style={{padding: 24, background: '#fff', minHeight: 360}}>
-                                Bill is a cat.
-                            </div>
+                        <Content className="content">
+
                         </Content>
                         <Footer style={{textAlign: 'center'}}>
                             @Author <a href="https://github.com/DevWichrowski">DevWichrowski</a>
