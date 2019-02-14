90cd869fb979f00cb407a1f72fbb52a1dda09cd1
Patryk Wichrowski <wichrowski.patryk92@gmail.com>
2019-02-14 22:04:26 +0100

    Refactored Column Component

--- src/components/Categories/Categories.js
+++ src/components/Categories/Categories.js
@@ -1,22 +1,28 @@
-import React, { Component } from 'react'
-import './Categories.scss'
-import AddColumn from './AddColumn/AddColumn';
+import React, { Component } from 'react';
+import './Categories.scss';
+import CategoryColumn from './CategoryColumn/CategoryColumn';
+import { connect } from 'react-redux';
 
-export default class Categories extends Component {
-  render() {
-    return (
-      <div className="Categories">
-        <h1>Kategorie operacji</h1>
-      <div className="categories-container">
-      <div className="left-column">
-      <AddColumn />
-      </div>
-      <div className="right-column">
-      
-      </div>
-      </div>
-        
-      </div>
-    )
-  }
+class Categories extends Component {
+	render() {
+		return (
+			<div className="Categories">
+				<h1> Kategorie operacji </h1>
+				<div className="categories-container">
+					<div className="left-column">
+						<CategoryColumn dataSource={this.props.balanceInfo.addCategories} arrow="arrow-up"/>
+					</div>
+					<div className="right-column">
+                    <CategoryColumn dataSource={this.props.balanceInfo.withdrawCategories} arrow="arrow-down"/>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
+
+const mapStateToProps = (state) => ({
+	balanceInfo: state.balanceInfo
+});
+
+export default connect(mapStateToProps, null)(Categories);
---
+++ src/components/Categories/CategoryColumn/CategoryColumn.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './CategoryColumn.scss';
+import {List, Icon} from 'antd';
+
+export default function CategoryColumn(props) {
+	return (
+		<div className="CategoryColumn">
+			<List
+				header={
+					<div>
+						<Icon id={`${props.arrow}`} type={`${props.arrow}`} /> <strong>Kategorie wpłat</strong>
+					</div>
+				}
+				bordered
+				dataSource={props.dataSource}
+				renderItem={(item) => <List.Item> {item} </List.Item>}
+			/>
+		</div>
+	);
+}
