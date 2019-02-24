72d90af92b02cae97ff95d742fa43578e099d759
Patryk Wichrowski <wichrowski.patryk92@gmail.com>
2019-02-24 13:36:08 +0100

    Add TransferModal Component

---
+++ src/components/Balance/TransferModal/TransferModal.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './TransferModal.scss';
+
+export default class TransferModal extends Component {
+  render() {
+    return (
+      <div className="TransferModal">
+        
+      </div>
+    )
+  }
+}
