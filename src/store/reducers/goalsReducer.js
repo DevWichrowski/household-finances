cd8a1f6f0f818d510a6fb57cdc4fd949dfcb1923
Patryk Wichrowski <wichrowski.patryk92@gmail.com>
2019-03-09 14:34:36 +0100

    Add logic to GoalsAction.SEND_TO_GOAL_SUCCESS

--- src/store/reducers/goalsReducer.js
+++ src/store/reducers/goalsReducer.js
@@ -2,7 +2,7 @@ import * as GoalsAction from '../actions/goalsAction';
 
 const initialState = {
 	goals: [ { id: '_aweq13221', goalTitle: 'Na motocykl', fundsNeeded: 1000, currentFunds: 100 } ],
-	transferCount: [{goalTitle: 'Na motocykl', totalFunds: 100} ]
+	transferCount: [ { goalTitle: 'Na motocykl', totalFunds: 100 } ]
 };
 
 export function goalsReducer(state = initialState, action) {
@@ -19,7 +19,7 @@ export function goalsReducer(state = initialState, action) {
 						currentFunds: action.currentFunds
 					}
 				],
-				transferCount: [...state.transferCount, {goalTitle: action.payload.goalTitle, totalFunds: 0}]
+				transferCount: [ ...state.transferCount, { goalTitle: action.payload.goalTitle, totalFunds: 0 } ]
 			};
 		}
 
@@ -28,7 +28,26 @@ export function goalsReducer(state = initialState, action) {
 		}
 
 		case GoalsAction.SEND_TO_GOAL_SUCCESS: {
-			return { ...state, goals: state.goals.map(goal => goal.id === action.payload.id ? {...goal, currentFunds: goal.currentFunds + action.payload.funds}: goal)}
+			return {
+				...state,
+				goals: state.goals.map(
+					(goal) =>
+						goal.id === action.payload.id
+							? { ...goal, currentFunds: goal.currentFunds + action.payload.funds }
+							: goal
+				),
+				transferCount: state.transferCount.map((transfer) => {
+					if (transfer.goalTitle === action.payload.category) {
+						return {
+							...transfer,
+							totalFunds: transfer.totalFunds + action.payload.funds,
+							goalTitle: action.payload.category
+						}
+					} else {
+						return transfer;
+					}
+				})
+			};
 		}
 
 		default: {
