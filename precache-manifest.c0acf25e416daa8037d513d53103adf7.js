e99285db58f58c5e60208a8bc21f6542e70a2483
Patryk Wichrowski <wichrowski.patryk92@gmail.com>
2019-03-10 21:33:33 +0100

    Updates

--- precache-manifest.c0acf25e416daa8037d513d53103adf7.js
+++ precache-manifest.c0acf25e416daa8037d513d53103adf7.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/household-finances/static/js/runtime~main.5c72c891.js"
   },
   {
-    "revision": "b8f206fd9559238f5d2d",
-    "url": "/household-finances/static/js/main.6571c5cb.chunk.js"
+    "revision": "8a8f4efb95b937964d62",
+    "url": "/household-finances/static/js/main.05f25ab2.chunk.js"
   },
   {
     "revision": "dbbd5130f0ba96b3a5f7",
     "url": "/household-finances/static/js/2.da40bb89.chunk.js"
   },
   {
-    "revision": "b8f206fd9559238f5d2d",
+    "revision": "8a8f4efb95b937964d62",
     "url": "/household-finances/static/css/main.1997ee88.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/household-finances/static/css/2.3b9d2585.chunk.css"
   },
   {
-    "revision": "407dcd36e736abe5a81b5e79ecf65b5f",
+    "revision": "d315d559ace19028046bde98b550f859",
     "url": "/household-finances/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/household-finances/precache-manifest.9eb405e078d4dd39e3de2b9d54ea41e2.js"
+  "/household-finances/precache-manifest.c0acf25e416daa8037d513d53103adf7.js"
 );
 
 workbox.clientsClaim();
